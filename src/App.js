9201dc7e9e6eb2d74702cd652b8d4f59ec401f3c
ADOFO-09 <emmanueladofo011@gmail.com>
2023-06-22 05:05:10 +0100

    added Components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
 
 const App = () => {
     return (
       <div className="App">
         <Navigation />
-        {/* <Logo/>
-        <ImageLinkForm/>
+        <Logo/>
+        <Rank/>
+        <ImageLinkForm/> 
+        {/* 
         <FaceRecognition/>
         */}
       </div>
