a47a54e5647a3415a20762e699c7cc20a94b1df8
ADOFO-09 <emmanueladofo011@gmail.com>
2023-06-25 17:51:46 +0100

    added a component

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -3,9 +3,14 @@ import React from 'react';
 
 const Rank = () => {
   return (
-    <div>
-     
-    </div>
+    <div className='f3 white'>
+      <div>
+        {'Adofo, your current rank is.....'}
+      </div>
+      <div>
+        {'#5'}
+      </div>   
+    </div>    
   );
 }
 
