f5cc854772cba6d1e77100dfb357759e035709a5
ADOFO-09 <emmanueladofo011@gmail.com>
2023-06-27 04:50:37 +0100

    adding functionality and Clarifai API installation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Clarifai from 'clarifai';
 import ParticlesBg from 'particles-bg'
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
@@ -7,22 +8,43 @@ import Rank from './components/Rank/Rank';
 import './App.css';
 
 
+const app = new Clarifai.App({
+  apiKey: '80ded2b2a85642bca6e77e72f0425abe'
+ });
 
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      input:'',
+    }
+  }
 
-const App = () => {
+  onInputChange = (event) => {
+    console.log(event.target.value)
+  }
+
+  onButtonSubmit = () => {
+    console.log('click')
+  }
+  render() {
   return (
     <div className="App">
       <ParticlesBg
-        num={300} type="cobweb" bg={true} />
+        num={30} type="cobweb" bg={true} />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm  
+      onInputChange = {this.onInputChange} 
+      onButtonSubmit = {this.onButtonSubmit}
+      />
       {/*
             <FaceRecognition/>
             */}
     </div>
   );
+          }
 }
   
 export default App;
