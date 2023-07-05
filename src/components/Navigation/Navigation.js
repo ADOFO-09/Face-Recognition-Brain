7fc1cfe1d632185e9477581ad8a429029fb0d8a8
ADOFO-09 <emmanueladofo011@gmail.com>
2023-07-05 05:44:03 +0100

    minor change

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,15 +5,14 @@ if(isSignedIn){
   <nav style={{display: 'flex', justifyContent: 'flex-end'}}>  
     <p onClick = {() => onRouteChange('signin')} className='f3 link dim black underline pointer'>Sign Out</p>
   </nav>
-}else  {
+}else {
   return (
     <nav style={{display: 'flex', justifyContent: 'flex-end'}}>  
       <p onClick = {() => onRouteChange('signin')} className='f3 link dim black underline pointer'>Sign In</p>
       <p onClick = {() => onRouteChange('register')} className='f3 link dim black underline pointer'>Register</p>
     </nav>
   );
-}
- 
-}
+} 
+ }
 
 export default Navigation;
