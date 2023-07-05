82bafbc98e347ed9fa8209788c7ecac3b0ae5b1a
ADOFO-09 <emmanueladofo011@gmail.com>
2023-07-05 05:42:02 +0100

    added register component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Clarifai from 'clarifai';
 import ParticlesBg from 'particles-bg'
 import Navigation from './components/Navigation/Navigation';
 import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -21,7 +22,8 @@ class App extends React.Component {
       input:'',
       imageUrl: '',
       box: {},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
@@ -98,6 +100,11 @@ class App extends React.Component {
   
 
   onRouteChange = (route) => {
+    if(route === 'signout'){
+      this.setState({isSignedIn: false});
+    } else if(route === 'home'){
+      this.setState({isSignedIn: true});
+    }
     this.setState({route: route});
   }
 
@@ -106,10 +113,9 @@ return (
      <div className="App">
         <ParticlesBg
           num={30} type="cobweb" bg={true} />
-        <Navigation onRouteChange={this.onRouteChange} />
-        { this.state.route === 'signin'
-          ?<Signin onRouteChange ={this.onRouteChange} />
-          :<div>
+        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange} />
+        { this.state.route === 'home'
+          ?<div>
             <Logo />
             <Rank />
             <ImageLinkForm  
@@ -118,6 +124,11 @@ return (
             />
             <FaceRecognition imageUrl={this.state.imageUrl}/>
           </div>
+          :(
+            this.state.route === 'signin'
+            ?<Signin onRouteChange={this.onRouteChange} />
+            :<Register onRouteChange={this.onRouteChange}/>
+          )
           
         }  
       </div>
