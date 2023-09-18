758b9b1a50a06fb89e2bdb4b4273bbdb5df46bbc
ADOFO-09 <emmanueladofo011@gmail.com>
2023-09-18 15:46:27 +0100

    Security Review: Moved API from frontend to Backend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Clarifai from 'clarifai';
 import ParticlesBg from 'particles-bg'
 import Navigation from './components/Navigation/Navigation';
 import Signin from './components/Signin/Signin';
@@ -8,12 +7,11 @@ import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import axios from 'axios';
 import './App.css';
 
 
-const app = new Clarifai.App({
-  apiKey: 'dc8af10eb7e34f5da10ffa571c609e9f'
- });
+
 
 const intialState = {
     input:'',
@@ -69,63 +67,14 @@ class App extends React.Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    console.log('click')  
-      //help me => user_id can be found in multiple ways, one way is in https://portal.clarifai.com/settings/profile 
-    const USER_ID = "dev-id";
-
-    
-    // Your PAT (Personal Access Token) can be found in the portal under Authentification
-    // help me => PAT can be found in https://portal.clarifai.com/settings/authentication (create one if necessary!)
-    const PAT = "804b3b17ea1245de94df8c10382621b2"; 
-    
-    
-    // help me => App Id is just the name of your app on the portal. 
-    const APP_ID = "second-application"; 
-
-
-    // Change these to whatever model and image input you want to use
-    // help me => https://help.clarifai.com/hc/en-us/articles/1500007677141-Where-to-find-your-Model-IDs-and-Model-Version-IDs
-    const MODEL_ID = "face-detection";
-    const MODEL_VERSION_ID = "6dc7e46bc9124c5c8824be4822abe105";
-
-    const IMAGE_URL = this.state.input;
-
-    ///////////////////////////////////////////////////////////////////////////////////
-    // YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
-    ///////////////////////////////////////////////////////////////////////////////////
-    const raw = JSON.stringify({
-      user_app_id: {
-        user_id: USER_ID,
-        app_id: APP_ID,
-      },
-      inputs: [
-        {
-          data: {
-            image: {
-              url: IMAGE_URL,
-            },
-          },
-        },
-      ],
-    });
-
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        Authorization: "Key " + PAT,
-      },
-      body: raw,
-    };
-
-    fetch(
-      "https://api.clarifai.com/v2/models/" +
-        MODEL_ID +
-        "/versions/" +
-        MODEL_VERSION_ID +
-        "/outputs",
-      requestOptions
-    )
+    fetch('http://localhost:3000/imageurl',{
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+          input: this.state.input    
+      })
+    })  
+    .then(response => response.json())
     .then(response => {
       if (response) {
         fetch('http://localhost:3000/image',{
